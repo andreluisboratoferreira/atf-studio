edb49dc2509053af92b664244deab8763b6085d2
André <andre.borato.ferreira@gmnail.com>
2026-07-08 19:54:37 -0300

    Primeiro Commit

---
+++ backend/main.py
@@ -0,0 +1,7 @@
+print("AutoFLow Iniciado")
+
+def criar_projeto(nome): 
+    print(f"Projeto '{nome}' criado com sucesso!")
+
+
+criar_projeto("Primeira edit")
---
+++ frontend/app.js
@@ -0,0 +1,5 @@
+const newproject = document.getElementById("btn-project")
+
+newproject.onclick = function() {
+    document.getElementById("text").innerHTML = "Novo Projeto Criado!"
+};
